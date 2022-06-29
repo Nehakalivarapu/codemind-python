106d552b8f9213b7d4d302033a2e57f78479ca9d
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-29 21:03:37 +0530

    a=int(input()) c=0 r=0 j=0 p=a k=a**2 t=a rev=0 while a:     d=a%10     c+=1     a=a//10 while k:     d=k%10     j+=1     if(j<=c):         rev=rev*10+d     k=k//10 while rev:     d=rev%10     r=r*10+d     rev=rev//10 if r==p:     print("Automorphic Number") else:     print("Not an Automorphic Number")

---
+++ Automorphic_Number.py
@@ -0,0 +1,26 @@
+a=int(input())
+c=0
+r=0
+j=0
+p=a
+k=a**2
+t=a
+rev=0
+while a:
+    d=a%10
+    c+=1
+    a=a//10
+while k:
+    d=k%10
+    j+=1
+    if(j<=c):
+        rev=rev*10+d
+    k=k//10
+while rev:
+    d=rev%10
+    r=r*10+d
+    rev=rev//10
+if r==p:
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
