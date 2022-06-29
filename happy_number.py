a4583fe256fe281e6aff1130cf39f73df0e13399
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-29 21:23:41 +0530

    n=int(input()) t=n r=0 while t:     d=t%10     r=r+(d**2)     t=t//10 while r>=10:     r2=r     r=0     while r2:         d=r2%10         r=r+(d**2)         r2=r2//10 if r==1 or r==7:     print("True") else:     print("False")

---
+++ happy_number.py
@@ -0,0 +1,18 @@
+n=int(input())
+t=n
+r=0
+while t:
+    d=t%10
+    r=r+(d**2)
+    t=t//10
+while r>=10:
+    r2=r
+    r=0
+    while r2:
+        d=r2%10
+        r=r+(d**2)
+        r2=r2//10
+if r==1 or r==7:
+    print("True")
+else:
+    print("False")
