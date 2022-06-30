ff1716dc7ff9ad99bec3b7bcf2e98c6de820b9a1
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-30 22:37:25 +0530

    a,b=map(int,input().split()) k=a*b for i in range(a,0,-1):     if a%i==0 and b%i==0:         print(i)         break

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,8 +1,6 @@
 a,b=map(int,input().split())
-gcd=0
-i=1
-while i<=a and i<=b:
+k=a*b
+for i in range(a,0,-1):
     if a%i==0 and b%i==0:
-        gcd=i
-    i+=1
-print(gcd)
+        print(i)
+        break
