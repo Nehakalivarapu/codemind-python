d2d4f8860e6c0b651839bfa0ca1f6567d550cf5f
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-30 21:08:03 +0530

    t=int(input()) for i in range(t):     k=int(input())     for i in range(k,2,-1):         for j in range(2,i):             if i%j==0:                 break         else:             break     for m in range(k+1,k+100):         for n in range(2,m):             if m%n==0:                 break         else:             break     if abs(k-m)>=abs(k-i):         print(i)     else:         print(m)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,21 +1,19 @@
-def prime(n):
-    for i in range(2,n):
-        if n%i==0:
-            return False
-    else:
-        return True
-k=int(input())
-for i in range(k):
-    a=int(input())
-    for j in range(a,2,-1):
-        if prime(j):
-            n=j
+t=int(input())
+for i in range(t):
+    k=int(input())
+    for i in range(k,2,-1):
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
             break
-    for p in range(a,a+100):
-         if prime(p):
-             m=p
-             break
-    if abs(n-a)<=abs(m-a):
-        print(n)
+    for m in range(k+1,k+100):
+        for n in range(2,m):
+            if m%n==0:
+                break
+        else:
+            break
+    if abs(k-m)>=abs(k-i):
+        print(i)
     else:
         print(m)
