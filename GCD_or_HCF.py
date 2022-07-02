ad4e8125bccab0a16a5b6f5b12e97e7026c5fa77
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-07-02 10:57:01 +0530

    def gcd(a,b):     for i in range(a,0,-1):         if a%i==0 and b%i==0:             return i a,b=map(int,input().split()) if(a>b):     a,b=b,a print(gcd(a,b))

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,6 +1,8 @@
+def gcd(a,b):
+    for i in range(a,0,-1):
+        if a%i==0 and b%i==0:
+            return i
 a,b=map(int,input().split())
-k=a*b
-for i in range(a,0,-1):
-    if a%i==0 and b%i==0:
-        print(i)
-        break
+if(a>b):
+    a,b=b,a
+print(gcd(a,b))
