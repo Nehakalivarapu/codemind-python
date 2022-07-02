7203e03252cae40e65accc4f21d19d89b8856910
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-07-02 20:14:01 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     c=0     if i==0:         c=1         b.append(c)         continue     if i<0:         i=i*(-1)     while i:         d=i%10         c+=1         i//=10     b.append(c) print(b.count(min(b)))

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,17 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+for i in a:
+    c=0
+    if i==0:
+        c=1
+        b.append(c)
+        continue
+    if i<0:
+        i=i*(-1)
+    while i:
+        d=i%10
+        c+=1
+        i//=10
+    b.append(c)
+print(b.count(min(b)))   
